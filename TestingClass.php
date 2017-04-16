e3c01d6671afc34221691ee2b72f474138459938
nsama86 <ja.ne.hw.1986@gmail.com>
2017-04-16 13:22:08 -0700

    Initial commit

---
+++ TestingClass.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: janeh
+ * Date: 4/15/2017
+ * Time: 4:19 PM
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class TestingClass extends TestCase
+{
+
+  public function testOne(){
+    $stack = [];
+
+    $this->assertEquals(0, count($stack));
+  }
+
+  public function testTwo(){
+    $stack = ["Foo"];
+
+    $this->assertEquals(1, count($stack));
+  }
+
+  /*
+  public function testThree(){
+    $stack = ["Foo"];
+
+    $this->assertEquals(0, count($stack));
+  }*/
+
+}
